7e5f4dd344349ba4c547114dd148d655f8b82147
David Dimitrov <david.dimitrov@educa.madrid.org>
2025-10-22 21:08:53 +0200

    fix(clear-not-working-unix): Changed the way the program clears the terminal. This works everywhere except on Windows bc the cmd doesn't support ANSI codes by default

--- src/Visual.java
+++ src/Visual.java
@@ -77,7 +77,8 @@ public class Visual {
             if (System.getProperty("os.name").contains("Windows")) {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } else {
-                new ProcessBuilder("clear").start();
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
             }
         } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
