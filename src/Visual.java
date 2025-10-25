d4adaa9a2edb4981c13e3cf292317b4cce5b4b16
David Dimitrov <david.dimitrov@educa.madrid.org>
2025-10-25 22:56:13 +0200

    release(v4.2): Removed useless import, updated gitignore andcompiled the v4.2 version

--- src/Visual.java
+++ src/Visual.java
@@ -17,7 +17,7 @@ public class Visual {
     public static String AT = color2 + "@" + color1;
 
     public static void showTitle() {
-        System.out.println(color1 + "/////////////////// K-WatchManager v4.0.1 /////////////////// || github.com/dev-dimitrov\n" + END);
+        System.out.println(color1 + "/////////// K-WatchManager v4.2 /////////// || github.com/dev-dimitrov\n" + END);
     }
 
     public static void showMain() {
